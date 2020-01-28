4dfb44934c8ca9f4fd984263392a9370543c7590
Paul Debevec <paulmdebevec@gmail.com>
2020-01-28 11:00:22 -0700

    Add new and create methods into controller. Add new form view and routes

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -6,4 +6,24 @@ class SheltersController < ApplicationController
   def show
     @shelter = Shelter.find(params[:shelter_id])
   end
+
+  def new
+  end
+
+  def create
+    shelter = Shelter.new(shelter_params)
+    if shelter.save
+      flash[:success] = "#{shelter.name} Was Successfully Created!"
+      redirect_to '/shelters'
+    else
+      flash[:error] = shelter.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  private
+
+  def shelter_params
+    params.permit(:name, :address, :city, :state, :zip)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/shelters', to: 'shelters#index'
+  post '/shelters', to: 'shelters#create'
+  get '/shelters/new', to: 'shelters#new'
   get '/shelters/:shelter_id', to: 'shelters#show'
-  
+
 end
