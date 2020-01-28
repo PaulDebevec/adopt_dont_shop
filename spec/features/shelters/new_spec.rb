bfdbc4944105a486df41639d0668630c5ed69f54
Paul Debevec <paulmdebevec@gmail.com>
2020-01-28 10:59:08 -0700

    Add test for a new shelter

--- spec/features/shelters/new_spec.rb
+++ spec/features/shelters/new_spec.rb
@@ -1,21 +1,3 @@
-# User Story 4, Shelter Creation
-#
-# As a visitor
-# When I visit the Shelter Index page
-# Then I see a link to create a new Shelter, "New Shelter"
-# When I click this link
-# Then I am taken to '/shelters/new' where I  see a form for a new shelter
-# When I fill out the form with a new shelter's:
-# - name
-# - address
-# - city
-# - state
-# - zip
-# And I click the button "Create Shelter" to submit the form
-# Then a `POST` request is sent to '/shelters',
-# a new shelter is created,
-# and I am redirected to the Shelter Index page where I see the new Shelter listed.
-
  require 'rails_helper'
 
  describe 'As a visitor when I visit the shelter index page I see a link to add
@@ -25,26 +7,32 @@
     visit '/shelters'
     click_link "Add New Shelter"
 
+    # fill in form information
     expect(current_path).to eq('/shelters/new')
     fill_in :name, with: "Barkin' Doggos"
     fill_in :address, with: "5112 Barkin Plaza"
     fill_in :city, with: "Hollywood"
     fill_in :state, with: "CA"
     fill_in :zip, with: 20938
-    click_button "Create New Shelter"
+    click_on "Create New Shelter"
 
+    # Expect to be redirected to /shelters and display flash message
     expect(current_path).to eq('/shelters')
+    expect(page).to have_content("Barkin' Doggos Was Successfully Created!")
 
+    # Checking that the shelter created above was actually created
     new_shelter = Shelter.last
     within "#shelter-#{new_shelter.id}" do
       expect(page).to have_content(new_shelter.name)
       click_link("#{new_shelter.name}")
-      expect(current_path).to eq("/shelters/#{new_shelter.id}")
-      expect(page).to have_content("Barkin' Doggos")
-      expect(page).to have_content("5112 Barkin Plaza")
-      expect(page).to have_content("Hollywood")
-      expect(page).to have_content("CA")
-      expect(page).to have_content(20938)
     end
+
+    # Routing to show page to verify shelter creation
+    expect(current_path).to eq("/shelters/#{new_shelter.id}")
+    expect(page).to have_content("Barkin' Doggos")
+    expect(page).to have_content("5112 Barkin Plaza")
+    expect(page).to have_content("Hollywood")
+    expect(page).to have_content("CA")
+    expect(page).to have_content(20938)
   end
 end
