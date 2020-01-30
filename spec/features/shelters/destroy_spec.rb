70ca1bdd42d0a7c7b2852a20c13791d20d98f53b
Paul Debevec <paulmdebevec@gmail.com>
2020-01-29 23:07:33 -0700

    Add shelter delete and edit under each shelter on shelter index page

--- spec/features/shelters/destroy_spec.rb
+++ spec/features/shelters/destroy_spec.rb
@@ -7,13 +7,25 @@ describe "Delete a shelter" do
       @fantastic_friends = Shelter.create(name: "Fantastic friends", address: "9872 Friendly Lane", city: "New York City", state: "NY", zip: 10001)
       @boulder_rescue = Shelter.create(name: "Boulder Rescue", address: "2712 Slobbery Circle", city: "Boulder", state: "CO", zip: 80205)
 
-      visit "/shelters/#{@boulder_rescue.id}"
     end
 
-    it "destroys the shelter, and it is no longer shown on the index page." do
-      click_link "Delete #{@boulder_rescue.name}"
+    it "Shelter show page delete functionality" do
+      visit "/shelters/#{@boulder_rescue.id}"
+
+      click_link "Delete Shelter"
       expect(current_path).to eq('/shelters')
       expect(page).to_not have_content("Boulder Rescue")
+    end
+
+    it "Shelter index page shows delete button next to each listed shelter" do
+      visit "/shelters"
+
+      within "#shelter-#{@fantastic_friends.id}" do
+        click_link "Delete Shelter"
+      end
+      
+      expect(current_path).to eq('/shelters')
+      expect(page).to_not have_content("Fantastic friends")
     end
   end
 end
